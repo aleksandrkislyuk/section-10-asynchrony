d199e171c26be9d2f83f26dcb1b6c797ba5639fc
Aleksandr Kislyuk <sr28@inbox.ru>
2024-12-11 19:52:20 +0400

    fifth commit

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 document.addEventListener('DOMContentLoaded', loadProducts);
 
 function loadProducts() {
-    fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(data => {
-            const productsContainer = document.querySelector('#products-container');
-            data.forEach(product => {
-                const productCard = document.createElement('div');
-                productCard.classList.add('product-card');
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      const productsContainer = document.querySelector('.products-container');
+      data.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('productCard');
 
-                productCard.innerHTML = `
-                    <h3>Название товара:${product.title}</h3>
-                    <p>Идентификатор товара: ${product.id}</p>
-                    <p>Стоимость товара: $${product.price}</p>
-                    <p>Категория товара: ${product.category}</p>
-                    <p>Описание товара: ${product.description}</p>
-                    <img src=${product.image} alt="${product.title}">
+        productCard.innerHTML = `
+                <div class="card">
+                    <h3>Название:${product.title}</h3>
+                    <p>Артикул: ${product.id}</p>
+                    <p>Цена: $${product.price}</p>
+                    <p>Категория: ${product.category}</p>
+                    <p>Описание: ${product.description}</p>
+                    <img src=${product.image}>
+                </div>
                 `;
 
-                productsContainer.appendChild(productCard);
-            });
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-        });
+        productsContainer.appendChild(productCard);
+      });
+    })
+    .catch(error => {
+      console.error('Ошибка:', error);
+    });
 }
+
